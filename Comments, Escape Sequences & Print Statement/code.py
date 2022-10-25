98d90ea28f5f8a8b8c2b9c879a31068694906824
Tanjela Ahana <ahanatanjela@gmail.com>
2022-10-25 12:42:59 +0600

    Add files via upload

---
+++ Comments, Escape Sequences & Print Statement/code.py
@@ -0,0 +1,10 @@
+#hello there
+"""
+This is a multiline comment
+"""
+
+
+print("I love you Tanim so much ", end="& ") #,end="" for marging two print statement
+print("I want to live with you")
+print("I want dresses and \t shoes")         # /t= tab
+print("I want dresses and \n shoes")         # /n= new line

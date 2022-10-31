0f86cad9ba5e54424338637b57cf410e8e7e4425
Tanjela Ahana <ahanatanjela@gmail.com>
2022-10-31 15:17:12 +0600

    Add files via upload

---
+++ Function/code2.py
@@ -0,0 +1,32 @@
+mystr = "i'm a good poor sad girl"
+
+#checking alpha numeric or not
+print(mystr.isalnum())
+
+#checking if alpha or not
+print(mystr.isalpha())
+
+#ends with function
+print(mystr.endswith("girl"))
+
+#counting letter
+print(mystr.count("a"))
+
+#making starting letter capital
+print(mystr.capitalize())
+
+#finding strings
+print(mystr.find("a"))
+
+#lower case
+print(mystr.lower())
+
+#upper case
+print(mystr.upper())
+
+#replacing string
+print(mystr.replace("a","an"))
+
+
+
+

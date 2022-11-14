9eac8b5f22d895cfc8d36460c1344ef9cfe89c02
Tanjela Ahana <ahanatanjela@gmail.com>
2022-11-14 23:53:16 +0600

    Add files via upload

---
+++ tuples/swaping.py
@@ -0,0 +1,14 @@
+#traditional swapping
+a = 8
+b = 5
+temp = a
+a = b
+b = temp
+print(a,b)
+
+#python swapping
+
+x = 10
+y = 12
+x, y = y, x
+print(x,y)
---
+++ tuples/tuples.py
@@ -0,0 +1,7 @@
+#TUPLES ARE IMMUTABLE WHICH MEANS IT CAN NOT BE CHANGED
+tp = (1,2,3)
+print(tp)
+
+#one tuple
+tp1 = (1,)
+print(tp1)
